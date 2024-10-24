b6ca91e8d8d85b806012127bfe927db9d01ebf3b
reigen002 <145865024+reigen002@users.noreply.github.com>
2024-10-24 11:15:31 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+# Your code continues here...
+
 
 # Set up Webdriver using WebDriver Manager to automatically manage ChromeDriver
 driver = webdriver.Chrome(ChromeDriverManager().install())
