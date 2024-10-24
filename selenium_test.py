7ad70374d0115a02775deb9a41fa415ff2c3aa4e
reigen002 <145865024+reigen002@users.noreply.github.com>
2024-10-24 11:10:51 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,20 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-#Set up Webdriver (This example uses Chrome)
-driver = webdriver.Chrome()
+# Set up Webdriver using WebDriver Manager to automatically manage ChromeDriver
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-#Open Google
+# Open Google
 driver.get("https://www.google.com")
 
-#Find the search box using its name attribute
-search_box = driver.find_element("name","q")
+# Find the search box using its name attribute
+search_box = driver.find_element("name", "q")
 
-#Type a search query
+# Type a search query
 search_box.send_keys("Selenium testing in Windows")
 
-#Simulate pressing the Enter key
+# Simulate pressing the Enter key
 search_box.send_keys(Keys.RETURN)
 
-#Close the browser
+# Close the browser
 driver.quit()
